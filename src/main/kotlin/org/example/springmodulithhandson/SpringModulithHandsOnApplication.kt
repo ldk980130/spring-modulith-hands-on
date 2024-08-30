58b2fc168d7c01353b74bef3874512e775fbab76
does <ldk980130@gmail.com>
2024-08-31 00:07:27 +0900

    project init

---
+++ src/main/kotlin/org/example/springmodulithhandson/SpringModulithHandsOnApplication.kt
@@ -0,0 +1,11 @@
+package org.example.springmodulithhandson
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringModulithHandsOnApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringModulithHandsOnApplication>(*args)
+}
