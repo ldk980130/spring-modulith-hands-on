430a6ea42dfc0e9de8d76974aca889b57cc59897
does <ldk980130@gmail.com>
2024-09-08 17:01:24 +0900

    애그리거트 루트는 다른 모듈에 오픈

---
+++ src/main/kotlin/org/example/springmodulithhandson/notification/domain/root/AggregateRootModuleConfig.kt
@@ -0,0 +1,9 @@
+package org.example.springmodulithhandson.notification.domain.root
+
+import org.springframework.modulith.NamedInterface
+import org.springframework.modulith.PackageInfo
+
+@Suppress("unused")
+@PackageInfo
+@NamedInterface("root")
+class AggregateRootModuleConfig
